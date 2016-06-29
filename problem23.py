6a996dc1b93d580cfd912e986fc355748ed7d597
Pei-Chen Huang <peichen.huang@tw.synaptics.com>
2016-06-29 14:42:36 +0800

    p23 on going

--- problem23.py
+++ problem23.py
@@ -3,18 +3,32 @@ from FindProperDivisors import findProperDivisorsSum as PSum
 Total = [i for i in range(1, 28124)]
 
 abundantList = list()
+Oddabundant = list()
 
-for i in range(1, 28123):
+for i in range(12, 28123):
     if PSum(i) > i:
         abundantList.append(i)
 
-length = len(abundantList)
-for i in range(length - 1, -1, -1):
-    for j in range(i+1):
-        asum = abundantList[i] + abundantList[j]
-        if asum > 28123:
-            break
-        elif asum in Total:
-            Total.remove(asum)
+for i in abundantList:
+    if (i + 12) in Total:
+        Total.remove(i + 12)
+    if i % 2 == 1:
+        Oddabundant.append(i)
+
+for i in Total:
+    if i % 2 == 0:
+        index = 1
+        while abundantList[index] <= (i / 2):
+            if (i - abundantList[index]) in abundantList:
+                Total.remove(i)
+                break
+            index += 1
+    else:
+        index = 0
+        while Oddabundant[index] < i:
+            if(i - Oddabundant[index]) in abundantList:
+                Total.remove(i)
+                break
+            index += 1
 
 print(sum(Total))
